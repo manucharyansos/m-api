222a2241e7459ef3df04a7cca4d7a9c16937953f
Sos <sosmanucharyan89@gmail.com>
2023-01-23 10:45:02 +0400

    ---

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -8,60 +8,9 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use App\Http\Controllers\API\ApiController;
 
 class AuthController extends BaseController
 {
-//    public function login(Request $request)
-//    {
-//        $data = $request->validate([
-//            'email' => 'required|string',
-//            'password' => 'required|string'
-//        ]);
-//
-//        $user = User::where('email', $data['email'])->first();
-//
-//        if (!$user || !Hash::check($data['password'], $user->password)) {
-//            return response([
-//                'msg' => 'incorrect username or password'
-//            ], 401);
-//        }
-//
-//        $token = $user->createToken('apiToken')->plainTextToken;
-//
-//        $res = [
-//            'user' => $user,
-//            'token' => $token
-//        ];
-//
-//        return response($res, 201);
-//    }
-
-//    public function register(Request $request)
-//    {
-//        $validator = Validator::make($request->all(), [
-//            'name' => 'required',
-//            'email' => 'required|email|unique:users,email',
-//            'password' => 'required',
-//            'confirm_password' => 'required|same:password',
-//        ]);
-//
-//        if($validator->fails()){
-//            return $this->errorResponse('Validation error.', $validator->errors(), 400);
-//        }
-//
-//        $data = $request->all();
-//        $data['password'] = bcrypt($data['password']);
-//        $user = User::create($data);
-//
-//        $token = $user->createToken('apiToken')->plainTextToken;
-//
-//        $res = [
-//            'user' => $user,
-//            'token' => $token
-//        ];
-//        return response()->json($res, 201);
-//    }
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
